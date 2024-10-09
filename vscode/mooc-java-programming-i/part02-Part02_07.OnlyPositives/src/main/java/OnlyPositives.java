e727bdee698c617be848f0dc10eb32cdf896dc50
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-09 16:37:27 -0500

    Add part02-Part02_07.OnlyPositives

--- vscode/mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ vscode/mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,22 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            } 
+
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+                continue;
+            }
+
+            int pw = (int) Math.pow(number, 2);
+            System.out.println(pw);
+            
+        }
     }
 }

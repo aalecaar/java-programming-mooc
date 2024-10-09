43eb7e8834e5349427c83656b488274b40db835f
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-09 15:41:30 -0500

    Add part02-Part02_03.AbsoluteValue

--- vscode/mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ vscode/mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,9 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        int absolute = Math.abs(number);
+        System.out.println(absolute);
     }
 }

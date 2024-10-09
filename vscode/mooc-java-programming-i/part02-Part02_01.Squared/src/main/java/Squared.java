e9f585532640656fc6c4c454bbe43761fa873932
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-08 21:19:26 -0500

    Add part02-Part02_01.Squared

--- vscode/mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
+++ vscode/mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,10 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        int squaredNumber = number * number;
+        System.out.println(squaredNumber);
 
     }
 }

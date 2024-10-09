1e478b9e5007f90f22e91d5cd93f68acfaebc3cb
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-09 17:15:31 -0500

    Add part02-Part02_13.AverageOfPositiveNumbers

--- vscode/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ vscode/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,31 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int positiveNumsCount = 0;
+        int positiveNumsSum = 0;
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number > 0) {
+                positiveNumsCount += 1;
+                positiveNumsSum += number;
+            }
+        }
+        
+        double average = (double) positiveNumsSum / positiveNumsCount;
+
+        if (positiveNumsCount == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println(average);
+        }
+
+
+
     }
 }

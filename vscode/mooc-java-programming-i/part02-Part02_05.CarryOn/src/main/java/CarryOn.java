f5663e11736c3fbd50095d44218ed8f10fdc68c5
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-09 16:03:19 -0500

    Add part02-Part02_05.CarryOn

--- vscode/mooc-java-programming-i/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ vscode/mooc-java-programming-i/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -5,6 +5,14 @@ public class CarryOn {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            System.out.println("Should we carry on? Type yes or no");
+            String answer = scanner.nextLine();
+            
+            if (answer.equalsIgnoreCase("no")) {
+                break;
+            }
+        }
+        
     }
 }

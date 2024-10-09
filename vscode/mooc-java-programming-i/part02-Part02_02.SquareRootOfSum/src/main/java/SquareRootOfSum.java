46a0df3356262f90667adeeb59a1a3995af7f920
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-09 15:37:09 -0500

    Add part02-Part02_02.SquareRootOfSum

--- vscode/mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ vscode/mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,12 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Enter the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        int sum = firstNumber + secondNumber;
+        double squaredRoot = Math.sqrt(sum);
+        System.out.println(squaredRoot);
     }
 }

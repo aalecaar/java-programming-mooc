f2f6860c4babec9c4b06559b1be7e46d89e1002f
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-09 17:08:23 -0500

    Add part02-Part02_12.AverageOfNumbers

--- vscode/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ vscode/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,19 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int numsCount = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+            sum += number;
+            numsCount += 1;
+        }
+        double average = (double) sum / numsCount;
+        System.out.println("Average of the numbers: " + average);
     }
 }

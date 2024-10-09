27642289a683c2db5c9b659d494859797fbd4dd6
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-09 17:04:17 -0500

    Add part02-Part02_11.NumberAndSumOfNumbers

--- vscode/mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ vscode/mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -6,5 +6,20 @@ public class NumberAndSumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int numsCount = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+            sum += number;
+            numsCount += 1;
+        }
+        System.out.println("Number of numbers: " + numsCount);
+        System.out.println("Sum of the numbers: " + sum);
+
     }
 }

e6bfe7609f9b842ff352186234ebe6fcc6fbd74e
Alejandro Cardona <alejo.98k@gmail.com>
2024-10-10 10:57:18 -0500

    Add part02-Part02_16.FromWhereToWhere

--- vscode/mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ vscode/mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,13 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Where to?");
+        System.out.println("Where from?");
+
+        int end = Integer.valueOf(scanner.nextLine());
+        int start = Integer.valueOf(scanner.nextLine());
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
     }
 }
